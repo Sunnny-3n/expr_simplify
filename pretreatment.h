76584fd37745b1cb457c18dc004f0773fa5c23b4
Sunnny-3n <amy2819106462@gmail.com>
2017-05-27 23:11:51 +0800

    预处理字符串

---
+++ pretreatment.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "define.h"
+
+#ifndef PRETREATMENT_H_
+#define PRETREATMENT_H_
+
+char *
+pretreatment(const char s[]);
+
+
+#endif
