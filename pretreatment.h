ce48437ac43f558d911e069947e0be7c209085c4
Sunnny-3n <amy2819106462@gmail.com>
2017-05-29 12:12:44 +0800

    预处理字符串

--- pretreatment.h
+++ pretreatment.h
@@ -1,11 +1,12 @@
+#ifndef PRETREATMENT_H_
+#define PRETREATMENT_H_
+
 #include <stdio.h>
 #include <ctype.h>
 #include <assert.h>
 #include <stdlib.h>
 #include "define.h"
 
-#ifndef PRETREATMENT_H_
-#define PRETREATMENT_H_
 
 char *
 pretreatment(const char s[]);
